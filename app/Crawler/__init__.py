5e4ae377febb86f93a41ad3f852bcc2e83fe0648
silentsvv <silentsvv@163.com>
2017-12-05 20:09:32 +0800

    add: limit

--- app/Crawler/__init__.py
+++ app/Crawler/__init__.py
@@ -1,7 +1,9 @@
 from urllib import request;
 from bs4 import BeautifulSoup;
 import sqlite3;
+import sys;
 
+sys.setrecursionlimit(5000)
 CrawInfo = []
 Current = set()
 LinkArr = set()
@@ -10,50 +12,64 @@ conn = sqlite3.connect('example.db')
 c = conn.cursor()
 
 def getWeb(url):
-    open = request.Request(url)
-    open.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-    open.add_header('Upgrade-Insecure-Requests', '1')
-    open.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
-    open.add_header('Accept-Language', 'zh-CN,zh;q=0.8')
-    response = request.urlopen(open)
-    OldArr.add(url)
-    with response as f:
-        html = f.read().decode('utf-8')
-        print(f.read().decode('utf-8'))
-        decodeHtml(html)
+    try:
+        open = request.Request(url)
+        open.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+        open.add_header('Upgrade-Insecure-Requests', '1')
+        open.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
+        open.add_header('Accept-Language', 'zh-CN,zh;q=0.8')
+        response = request.urlopen(open)
+        OldArr.add(url)
+        with response as f:
+            html = f.read().decode('utf-8')
+            print(f.read().decode('utf-8'))
+            decodeHtml(html)
+    except Exception:
+        print('爬取出错啦, 跳过获取')
+        newUrl = LinkArr.pop()
+        getWeb(newUrl)
 
 
 def decodeHtml(html):
     global LinkArr
     global Current
-    soup = BeautifulSoup(html, "html.parser")
-    dic = {}
+    global OldArr
 
-    if soup.select('.lemmaWgt-lemmaTitle-title h1').__len__() >= 0:
-        dic['title'] = soup.select('.lemmaWgt-lemmaTitle-title h1')[0].string
-    if soup.select('[label-module=lemmaSummary]').__len__() >= 0:
-        dic['intro'] = soup.select('[label-module=lemmaSummary]')[0].get_text()
-
-    CrawInfo.append(dic)
-
-    c.execute("INSERT INTO crawler (id, title, content) values(null,?,?)", (dic['title'], dic['intro']))
-    conn.commit()
-
-    links = soup.select('a[href^="/item/"]')
-    for link in links:
-        Current.add('https://baike.baidu.com' + link['href'])
-
-
-    if LinkArr.__len__() == 0:
-        LinkArr = Current
-        Current = set()
-
-    print(CrawInfo.__len__())
-    newUrl = LinkArr.pop()
-    if CrawInfo.__len__() <= 100:
+    if CrawInfo.__len__() <= 2000:
         try:
+            soup = BeautifulSoup(html, "html.parser")
+            dic = {}
+
+            if soup.select('.lemmaWgt-lemmaTitle-title h1').__len__() >= 0:
+                dic['title'] = soup.select('.lemmaWgt-lemmaTitle-title h1')[0].string
+            if soup.select('[label-module=lemmaSummary]').__len__() >= 0:
+                dic['intro'] = soup.select('[label-module=lemmaSummary]')[0].get_text()
+
+            CrawInfo.append(dic)
+
+            c.execute("INSERT INTO crawler (id, title, content) values(null,?,?)", (dic['title'], dic['intro']))
+            conn.commit()
+
+            links = soup.select('a[href^="/item/"]')
+            for link in links:
+                Current.add('https://baike.baidu.com' + link['href'])
+
+
+            if LinkArr.__len__() == 0:
+                LinkArr = Current
+                Current = set()
+
+            print('当前进度:' + str(CrawInfo.__len__()))
+            print('OldArr长度:' + str(OldArr.__len__()))
+            print('LinkArr长度' + str(LinkArr.__len__()))
+            print('Current长度' + str(Current.__len__()))
+            newUrl = LinkArr.pop()
+            OldArr.add(newUrl)
+
+
             print('爬取成功')
             getWeb(newUrl)
+
         except Exception:
             print('爬取出错啦, 跳过获取')
             newUrl = LinkArr.pop()
